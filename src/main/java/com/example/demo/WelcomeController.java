dfc1127a7e438c9aecf0668c405cfc807d2d11b6
Matthew Casperson <matthewcasperson@gmail.com>
2017-08-02 07:42:53 +1000

    Added a page count to test the the session handling

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -1,21 +1,31 @@
 package com.example.demo;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("pageCount")
 public class WelcomeController {
 
     // inject via application.properties
     @Value("${welcome.message:test}")
     private String message = "Hello World";
 
+    @ModelAttribute("pageCount")
+    AtomicInteger getPageCount() {
+        return new AtomicInteger();
+    }
+
     @RequestMapping("/")
-    public String welcome(Map<String, Object> model) {
+    public String welcome(@ModelAttribute("pageCount") AtomicInteger pageCount, Map<String, Object> model) {
         model.put("message", this.message);
+        model.put("count", pageCount.incrementAndGet());
         return "welcome";
     }
 
